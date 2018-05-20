cb415dae29f3c858e2c2072acf2f54162da11027
Peter Kurfer <peter.kurfer@googlemail.com>
2018-05-20 18:58:57 +0100

    Basic project structure

---
+++ src/test_fizzbuzz.py
@@ -0,0 +1,16 @@
+from fizzbuzz import fizz_buzz
+
+def test_3():
+    assert fizz_buzz(3) == "fizz"
+
+def test_5():
+    assert fizz_buzz(5) == "buzz"
+
+def test_13():
+    assert fizz_buzz(13) == "fizz"
+
+def test_15():
+    assert fizz_buzz(15) == "fizzbuzz"
+
+def test_30():
+    assert fizz_buzz(30) == "fizzbuzz"
